bb1dea4decefb324acea231ec00127e6a79ea855
raghuramperi <raghuram111@gmail.com>
2020-10-31 14:01:14 -0700

    Update ftat.java
    updated test ip

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -16,8 +16,8 @@ public class ftat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://35.233.186.183:8080/QAWebapp/";
-	public String URL1 = "http://35.233.186.183:8080/QAWebapp/avncreatepage.jsp";
+	public String URL = "http://104.40.90.76:8080/QAWebapp/";
+	public String URL1 = "http://104.40.90.76:8080/QAWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	 
